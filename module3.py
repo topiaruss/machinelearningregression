c967b5a2a6e0604e2ca8beb28e045b173f05e1d1
Russ Ferriday <russf@topia.com>
2015-09-26 11:43:48 +0100

    initial scatter plot

--- module3.py
+++ module3.py
@@ -8,9 +8,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
+
 bikes_df = pd.read_csv('./data/bikes_subsampled.csv')
 
 # We select the variables temperature and bikes_count
 
 temperature = bikes_df['temperature'].values
 bikes_count = bikes_df['count'].values
+
+plt.scatter(temperature, bikes_count, color='k')
+plt.xlabel('temperature')
+plt.ylabel('hired')
+plt.xlim(-5,40)
+plt.tight_layout()
+plt.show()
