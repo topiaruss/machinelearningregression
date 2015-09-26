fd5f1c919024994ea8b6af0285fec417d6a76ca2
Russ Ferriday <russf@topia.com>
2015-09-26 16:26:11 +0100

    to 37

--- module6.py
+++ module6.py
@@ -26,12 +26,23 @@ features = ['temperature', 'humidity', 'windspeed']
 X = bikes_df[features].values
 y = bikes_df['count'].values
 
-polynomial_ridge = PolynomialRidge(degree=2, alpha=0)
-polynomial_ridge.fit(X, y)
-coefs_0 = polynomial_ridge.steps[2][1].coef_
-print coefs_0
+# polynomial_ridge = PolynomialRidge(degree=2, alpha=0)
+# polynomial_ridge.fit(X, y)
+# coefs_0 = polynomial_ridge.steps[2][1].coef_
+# print coefs_0
+#
+# polynomial_ridge = PolynomialRidge(degree=2, alpha=1)
+# polynomial_ridge.fit(X, y)
+# coefs_1 = polynomial_ridge.steps[2][1].coef_
+# print coefs_1
 
-polynomial_ridge = PolynomialRidge(degree=2, alpha=1)
-polynomial_ridge.fit(X, y)
-coefs_1 = polynomial_ridge.steps[2][1].coef_
-print coefs_1
+coefs = []
+lspace = np.logspace(-3, 3, 100)
+for alpha in lspace:
+    polynomial_ridge = PolynomialRidge(degree=2, alpha=alpha)
+    polynomial_ridge.fit(X, y)
+    coefs.append(polynomial_ridge.steps[2][1].coef_)
+coefs = np.array(coefs)
+plt.plot(lspace, coefs)
+plt.gca().set_xscale('log')
+plt.show()
