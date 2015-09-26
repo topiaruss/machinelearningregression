89e4259fc522d6643ae1432c222fa2da3db7a0d5
Russ Ferriday <russf@topia.com>
2015-09-26 11:54:08 +0100

    Add initial fit by hand

--- module3.py
+++ module3.py
@@ -12,13 +12,16 @@ from sklearn.linear_model import LinearRegression
 bikes_df = pd.read_csv('./data/bikes_subsampled.csv')
 
 # We select the variables temperature and bikes_count
-
+a = 10
 temperature = bikes_df['temperature'].values
 bikes_count = bikes_df['count'].values
-
+temperature_predict = np.expand_dims(a=np.linspace(-5, 40, 100), axis=1)
+bikes_count_predict = a*temperature_predict
 plt.scatter(temperature, bikes_count, color='k')
+plt.plot(temperature_predict, bikes_count_predict, linewidth=2)
 plt.xlabel('temperature')
-plt.ylabel('hired')
+plt.ylabel('No. HIred')
 plt.xlim(-5,40)
 plt.tight_layout()
 plt.show()
+
